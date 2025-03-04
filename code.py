ec998525fbafb8de7b78c6346390bd30db2cb01d
Hyperplane2021 <zc.cao23@gmail.com>
2025-03-04 12:52:43 +0100

    Version function_07

--- code.py
+++ code.py
@@ -1,4 +1,10 @@
-def find_Max_Num(num_list):
-    num_list.sort(reverse=True)
-    max_num = int(''.join(map(str, num_list)))
-    return max_num
+def find_Max_Num(digits):
+    digits.sort()
+    max_num = ''
+    while digits:
+        while digits[0] == 0:
+            digits.pop(0)
+        if not digits:
+            break
+        max_num += str(digits.pop(0))
+    return int(max_num)
