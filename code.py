333f3651bb4272ad91622ae3f495bc9bd5d1adc9
Hyperplane2021 <zc.cao23@gmail.com>
2025-03-04 12:50:47 +0100

    Version function_02

--- code.py
+++ code.py
@@ -1,7 +1,3 @@
-def find_Max_Num(nums):
-    max_num = 0
-    for i in str(nums):
-        for j in str(nums):
-            if i!=j and int(str(nums).replace(i,'')) > int(str(nums).replace(j,'')):
-                return int(i+j+str(nums).replace(i,'').replace(j,''))
-    return max_num
+def find_Max_Num(digits):
+    max_number = [str(x) for x in sorted(digits, reverse=True)]
+    return int(''.join(max_number))
