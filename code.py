26978b314a6aabc185270a3539dbb5fe430a77b5
Hyperplane2021 <94168094+Hyperplane2021@users.noreply.github.com>
2025-03-11 13:58:07 +0100

    Update code.py

--- code.py
+++ code.py
@@ -5,3 +5,11 @@ def find_Max_Num(nums):
             if i!=j and int(str(nums).replace(i,'')) > int(str(nums).replace(j,'')):
                 return int(i+j+str(nums).replace(i,'').replace(j,''))
     return max_num
+
+###
+This function takes a single number as input 
+and returns the sum of the two digits that are not equal to each other. 
+If no such pair of digits exists, 
+it returns the maximum digit in the number.
+###
+
