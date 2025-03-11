b785122a8cef7bb33a4101e4ab46875bbe9f62dc
Hyperplane2021 <94168094+Hyperplane2021@users.noreply.github.com>
2025-03-11 14:00:24 +0100

    Update code.py

--- code.py
+++ code.py
@@ -7,3 +7,12 @@ def find_Max_Num(digits):
         if num > max_num:
             max_num = num
     return max_num
+
+###
+This function takes a list of digits 
+and returns the largest number that can be formed using these digits. 
+It does this by generating all possible permutations of the digits, 
+converting each permutation to an integer, 
+and keeping track of the largest number found.
+###
+
