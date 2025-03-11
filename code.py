e9578c4741c2efdd05a2714739aa71e05a7e3270
Hyperplane2021 <94168094+Hyperplane2021@users.noreply.github.com>
2025-03-11 13:59:27 +0100

    Update code.py

--- code.py
+++ code.py
@@ -3,3 +3,9 @@ def find_Max_Num(num_list):
     for i in range(len(num_list),0,-1):
         max_num = max_num + str(num_list[i-1])
     return int(max_num)
+
+###
+This function takes a list of numbers as an argument. 
+It then creates a new string by concatenating the numbers in the list in reverse order. 
+Finally, it converts this string back into an integer and returns it.
+###
