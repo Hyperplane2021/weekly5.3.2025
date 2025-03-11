bced556cdcf6e3027417e44e5865d1f0bbaa9456
Hyperplane2021 <94168094+Hyperplane2021@users.noreply.github.com>
2025-03-11 13:59:43 +0100

    Update code.py

--- code.py
+++ code.py
@@ -2,3 +2,10 @@ def find_Max_Num(num_list):
     num_list.sort(reverse=True)
     max_num = int(''.join(map(str, num_list)))
     return max_num
+
+###
+This function takes a list of numbers as input, 
+sorts them in descending order, 
+and then converts them into a single integer. 
+It returns this integer.
+###
