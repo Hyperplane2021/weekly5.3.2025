1902840becaf2b087556535848213690984803b8
Hyperplane2021 <94168094+Hyperplane2021@users.noreply.github.com>
2025-03-11 14:00:43 +0100

    Update code.py

--- code.py
+++ code.py
@@ -5,3 +5,13 @@ def find_Max_Num(lst):
         max_num += str(num)
     
     return int(max_num)
+
+
+###
+This function takes a list of numbers as input, 
+sorts them in descending order, 
+and then converts the list into a string. 
+It then converts this string back into an integer. 
+This effectively returns the largest number in the list.
+###
+
